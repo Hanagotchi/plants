287316ae100c6fddcede78d4ca5c8b107373f115
Agustinefe <agustinefe@hotmail.com>
2024-02-23 18:08:21 -0300

    HAN-53: update plant-type table

--- app/service/sql/init_tables.sh
+++ app/service/sql/init_tables.sh
@@ -6,6 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "plants" <<-'EOSQL'
 
     CREATE TABLE IF NOT EXISTS dev.plant_types (
         botanical_name VARCHAR(70) PRIMARY KEY,
+        id INT UNIQUE NOT NULL,
         common_name VARCHAR(70) NOT NULL,
         description VARCHAR(600) NOT NULL,
         cares VARCHAR(600) NOT NULL,
@@ -18,7 +19,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "plants" <<-'EOSQL'
             INSERT INTO dev.plant_types (botanical_name, common_name, description, cares, photo_link)
             VALUES 
                  (
-                'Streptocarpus', 
+                'Streptocarpus',
+                224, 
                 'Cabo Primrose',
                 'Su nombre común es Cabo Primrose, refiriéndose al nombre de varias especies de Sudáfrica y su semejanza superficial al género Primula. El género es nativo de partes de África y Madagascar (con unas pocas especies extrañas en Asia, que probablemente no tienen cabida en el género). Las plantas a menudo crecen en la sombra de las laderas rocosas o acantilados. Se encuentran cada vez más sobre el terreno, grietas de rocas, y la semilla puede germinar y crecer casi en cualquier parte.',
                 'Se desempeña mejor con luz brillante filtrada y sombra del sol abrasador en suelos fértiles, húmedos, ricos en humus y bien drenados. Riegue libremente permitiendo que la tierra se seque entre riegos. Alimente quincenalmente con un fertilizante con alto contenido de potasio.',
@@ -26,6 +28,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "plants" <<-'EOSQL'
             ),
             (
                 'Pilea microphylla',
+                198,
                 'Helecho Artilleria',
                 'Es una planta anual originaria de Florida, México, las Indias Occidentales y América Central y del Sur tropical. La planta pertenece a la familia Urticaceae.​ Tiene tallos de color verde claro, casi suculentos, y pequeñas hojas de 1/8 que contribuyen a su otro apodo, Helecho Artillería, aunque no está relacionado con los helechos.',
                 'Utilice tierra para macetas rica en materia orgánica, suelta, con buen drenaje y que retenga la humedad. Evite compactar la tierra con fuerza al plantar. En su lugar, apisónelo sin apretar y deje muchos orificios de aire para facilitar el flujo de nutrientes, oxígeno y agua. Fertilice con moderación para evitar la acumulación de sales y enjuague varias veces según sea necesario en caso de sobrealimentación. Mantenga una humedad uniforme sin sobresaturación regando cuando la superficie del suelo se sienta seca.',
@@ -33,6 +36,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "plants" <<-'EOSQL'
             ),
             (
                 'Pentas lanceolata',
+                187,
                 'Estrella egipcia',
                 'Es una especie de planta con flores (Angiosperma) perteneciente a la familia Rubiaceae, es nativa de gran parte de África así como de Yemen.​ Es conocida por su amplio uso como planta de jardín donde a menudo se la usa en los jardines para mariposas.',
                 'Si reciben abono para plantas de flor durante el periodo productivo y se pinzan las flores marchitas, la producción será más abundante. Se han de podar al final del invierno para que puedan rebrotar con fuerza. Las pentas pueden verse afectadas por los pulgones y la araña roja, que suelen combatirse con el mismo fitosanitario.',
@@ -40,6 +44,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "plants" <<-'EOSQL'
             ),
             (
                 'Ixora coccinea',
+                154,
                 'Planta coral',
                 'Es un pequeño arbusto con numerosas flores de pequeño tamaño que permanecen formando umbelas compuestas durante casi todo el año. Es originaria de Asia, específicamente del sur de la India y de Sri Lanka y es muy empleada en jardinería.',
                 'Requiere sol y agua en abundancia y es más feliz al aire libre a pleno sol y en un ambiente adecuadamente húmedo. Cuando se cultiva en interiores, se deben mantener estas condiciones subtropicales que conducirán a una planta de interior de Ixora coccinea saludable que florecerá durante todo el año.',
