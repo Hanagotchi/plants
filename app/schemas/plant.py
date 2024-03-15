60c620b640ff579e31fdcbcea6f92aa520111a1a
Federico Pacheco <fpacheco@fi.uba.ar>
2024-03-15 02:41:04 -0300

    Update plant schema and swagger documentation

--- app/schemas/plant.py
+++ app/schemas/plant.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 class PlantCreateSchema(BaseModel):
     id_user: int = Field(..., example=1)
     name: str = Field(..., example="Agus")
-    scientific_name: str = Field(..., example="Aguspolis grandis")
+    scientific_name: str = Field(..., example="Thunbergia alata")
 
 
 class PlantSchema(PlantCreateSchema):
