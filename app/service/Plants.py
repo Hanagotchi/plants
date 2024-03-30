609ada0c641808104008c995ae555953f28f1389
Agustinefe <agustin.firmapaz@creativecoefficient.net>
2024-03-30 15:42:11 -0300

    Remove unused prints

--- app/service/Plants.py
+++ app/service/Plants.py
@@ -54,8 +54,6 @@ class PlantsService():
         if month:
             left = date(year, month, 1)
             right = date(year+1, 1, 1) if month == 12 else date(year, month+1, 1)
-            print(left)
-            print(right)
         else:
             left = date(year, 1, 1)
             right = date(year+1, 1, 1)
